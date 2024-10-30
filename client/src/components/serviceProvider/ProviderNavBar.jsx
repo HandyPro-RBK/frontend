cfb1020c56a0e9bc7eda8dbd0bb1a4effc91588f
Medbesbess <groskhan@gmail.com>
2024-10-30 11:40:19 +0100

    dashboard provider + add popup in dashboard

--- client/src/components/serviceProvider/ProviderNavBar.jsx
+++ client/src/components/serviceProvider/ProviderNavBar.jsx
@@ -4,9 +4,23 @@ import { FaUserCircle } from "react-icons/fa";
 
 const ProviderNavBar = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [dropdownTimeout, setDropdownTimeout] = useState(null);
 
   const toggleDropdown = () => {
+    clearTimeout(dropdownTimeout);
     setDropdownOpen(!dropdownOpen);
+  };
+
+  const handleMouseEnter = () => {
+    clearTimeout(dropdownTimeout);
+    setDropdownOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    const timeout = setTimeout(() => {
+      setDropdownOpen(false);
+    }, 300); // Délai avant que le menu ne disparaisse
+    setDropdownTimeout(timeout);
   };
 
   return (
@@ -21,7 +35,7 @@ const ProviderNavBar = () => {
           className="h-8 mr-2"
         />
       </div>
-      <ul className="flex space-x-6 text-lg ml-2">
+      <ul className="flex space-x-6 text-lg ml-2 justify-center">
         <li>
           <Link to="/requests">Requests</Link>
         </li>
@@ -30,24 +44,23 @@ const ProviderNavBar = () => {
         </li>
         <li
           className="relative"
-          onMouseEnter={toggleDropdown}
-          onMouseLeave={toggleDropdown}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
         >
           <span className="cursor-pointer">
-            {" "}
             <FaUserCircle className="text-blue-900 text-3xl " />
           </span>
           {dropdownOpen && (
             <div className="absolute top-full mt-1 w-40 bg-white text-black border border-gray-200 shadow-lg rounded-md">
               <ul className="flex flex-col">
                 <li className="p-2 hover:bg-gray-100 cursor-pointer">
-                  <Link to="/categories">profile</Link>
+                  <Link to="/categories">Profile</Link>
                 </li>
                 <li className="p-2 hover:bg-gray-100 cursor-pointer">
-                  <Link to="/categories">edit profile</Link>
+                  <Link to="/categories">Edit Profile</Link>
                 </li>
                 <li className="p-2 hover:bg-gray-100 cursor-pointer">
-                  <Link to="/categories">logout</Link>
+                  <Link to="/categories">Logout</Link>
                 </li>
               </ul>
             </div>
